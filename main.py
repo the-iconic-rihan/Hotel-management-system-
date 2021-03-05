3f386801e69f05eda13889ffb6f976c9f2cf5a37
the-iconic-rihan <rihanbagwan5017@gmail.com>
2021-03-04 21:52:29 -0800

    second commit

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class Restaurant:
         self.hotel_name = hotel_name
         self.order = {}
 
+    # taking the orders of veg section
     def drinks(self, user_drink, size, customer_name):
         if user_drink in Drinks and size == 2 or 1:
             self.order.update({user_drink: size})
@@ -29,6 +30,7 @@ class Restaurant:
         else:
             print(f"Sorry, but '{user_veg}' is not available at the veg card.")
 
+    # taking the orders of non-veg section
     def non_veg_food(self, user_non_veg, size, customer_name):
         if user_non_veg in non_veg_food and size == 2 or 1:
             self.order.update({user_non_veg: size})
@@ -36,20 +38,69 @@ class Restaurant:
         else:
             print(f"Sorry, but '{user_non_veg}' is not available at the non-veg card.")
 
+    def non_veg_breakfast(self, user_non_veg, size, customer_name):
+        if user_non_veg in non_veg_breakfast and size == 2 or 1:
+            self.order.update({user_non_veg: size})
+            print(f"{size}:{user_non_veg} served to {customer_name}")
+        else:
+            print(f"Sorry, but '{user_non_veg}' is not available at the non-veg card.")
 
+    def non_veg_cocktail(self, user_non_veg, size, customer_name):
+        if user_non_veg in cocktails and size == 2 or 1:
+            self.order.update({user_non_veg: size})
+            print(f"{size}:{user_non_veg} served to {customer_name}")
+        else:
+            print(f"Sorry, but '{user_non_veg}' is not available at the non-veg card.")
+
+
+# displaying the vegetarian section
 def display_drinks():
+    print("List of Drinks in veg.")
     for index, drink in enumerate(Drinks):
         print(f"{index + 1}]{drink}")
 
 
 def display_veg_food():
+    print("List of veg-dishes.")
     for index, veg_items in enumerate(veg_food):
         print(f"{index + 1}]{veg_items}")
 
 
 def display_breakfast():
+    print("List of breakfast items.")
     for index, items in enumerate(breakfast):
         print(f"{index + 1}] {items}")
+
+
+# displaying the non-vegetarian section
+def display_cocktails():
+    print("List of Cocktails.")
+    for index, drink in enumerate(cocktails):
+        print(f"{index + 1}]{drink}")
+
+
+def display_non_veg_dishes():
+    print("List of Non-veg dishes.")
+    for index, dishes in enumerate(non_veg_food):
+        print(f"{index + 1}]{dishes}")
+
+
+def display_non_veg_breakfast():
+    print("List of Non-veg breakfast.")
+    for index, dishes in enumerate(non_veg_breakfast):
+        print(f"{index + 1}]{dishes}")
+
+
+def menu_card():
+    print("Here is the list of food varieties we have in have in our hotel.")
+    print("The vegetarian section :- ")
+    display_drinks()
+    display_breakfast()
+    display_veg_food()
+    print("The non-vegetarian section :-")
+    display_cocktails()
+    display_non_veg_breakfast()
+    display_non_veg_dishes()
 
 
 def main():
@@ -57,8 +108,7 @@ def main():
 
     print(f"Welcome to {hotel_name}")
     print("We have following varieties of food in our hotel :-", '%s' % ','.join(map(str, food_variety)))
-    # about to def the function
-
+    menu_card()
     print("Hit your choice ")
     food_type = int(input("\t1]Veg \n\t2] Non-veg \n\t3] Both \t\n: "))
     if food_type == 1:
@@ -66,21 +116,18 @@ def main():
                                       "&& BreakFast \n\tHit your choice : "))
         if food_type_variety == 1:
             _drinks_order()
-            # print(hotel_customer.order)
 
         elif food_type_variety == 2:
             _breakfast_order()
-            # print(hotel_customer.order)
 
         elif food_type_variety == 3:
             _dishes_order()
+
         elif food_type_variety == 4:
-            print("Here is the menu card of Drinks && Dishes respectively.")
             _drinks_order()
             _dishes_order()
 
         elif food_type_variety == 5:
-            print("Here is the menu card of breakfast && Drinks respectively")
             _breakfast_order()
             _drinks_order()
 
@@ -89,14 +136,39 @@ def main():
             main()
 
     elif food_type == 2:
-        food_type_variety = int(input("\t1]BreakFast \n\t2]Dishes \n\tHit your choice : "))
+        food_type_variety = int(input("\t1]BreakFast \n\t2]Dishes\n\t3]Cocktails \n\tHit your choice : "))
         if food_type_variety == 1:
             _breakfast_order()
         elif food_type_variety == 2:
             _dishes_order()
+        elif food_type_variety == 3:
+            _cocktails_order()
+            print(hotel_customer.order)
         else:
             print("You hit the wrong choice!")
             main()
+
+    elif food_type == 3:
+        food_type_variety = int(input("\t1] Drinks && Cocktails \n\t2] Veg && Non-veg breakfast "
+                                      "\n\t3] Non-veg && veg dishes" "\n\tHit your choice : "))
+        try:
+            if food_type_variety == 1:
+                print("We will go with Drinks and Cocktails")
+                _drinks_order()
+                _cocktails_order()
+            elif food_type_variety == 2:
+                print("We will go with veg and non-veg breakfast")
+                non_veg_breakfast_order()
+                _breakfast_order()
+            elif food_type_variety == 3:
+                print("We will go with veg and non-veg dishes")
+                non_veg_dishes_order()
+                _dishes_order()
+        except IndexError:
+            print("The input you gave is incorrect.")
+            main()
+        else:
+            print("All your orders are served , please enjoy the moment.")
 
     else:
         print("You hit the wrong choice!")
@@ -111,8 +183,7 @@ if __name__ == '__main__':
               "The Marble Queen": 5200}
     breakfast = {"Tea": 50,
                  "Breakfast Cereals": 100,
-                 "Eggs (Hot or Hard Boiled)": 250,
-                 "Meats (Sausage, Bacon, or Ham)": 500,
+                 "Pineapple Upside Down Pancakes.": 500,
                  "Porridge/Oatmeal.": 1000}
 
     veg_food = {
@@ -120,6 +191,19 @@ if __name__ == '__main__':
         "Quick Salted Caramel Pie": 600,
         "Prawn Pie": 5051,
         "Yorkshire Lamb Patties": 2000
+    }
+
+    non_veg_breakfast = {
+        "Scrambled Eggs": 100,
+        "Eggs (Hot or Hard Boiled)": 250,
+        "Meats (Sausage, Bacon, or Ham)": 500,
+        "Stuffed Omelet": 200
+    }
+    cocktails = {
+        "Mojito": 505,
+        "Daiquiri": 450,
+        "Bloody Mary": 650,
+        "Moscow Mule.": 750
     }
     non_veg_food = {
         "Bangers and Mash": 4500,
@@ -131,30 +215,44 @@ if __name__ == '__main__':
     hotel_customer = Restaurant(hotel_name)
     customer_name = str(input("Enter your name : "))
 
-
+    # veg order functioning
     def _drinks_order():
-        print("Here is the menu cart of Drinks")
-        display_drinks()
+        print("Select the drinks from menu card.")
         user_drink = str(input("What's your order in drinks ? \nEnter the drink name and no of drinks: "))
         size = int(input())
         hotel_customer.drinks(user_drink, size, customer_name)
 
 
     def _breakfast_order():
-        print("Here is the menu cart of breakfast")
-        display_breakfast()
-        user_breakfast = str(
-            input("What's your order in breakfast ? \nEnter the breakfast name and no of dishes: "))
+        print("Select the breakfast dish from menu card.")
+        user_breakfast = str(input("What's your order in breakfast ? \nEnter the breakfast name and no of dishes: "))
         size = int(input())
         hotel_customer.breakfast(user_breakfast, size, customer_name)
 
 
     def _dishes_order():
-        print("Here is the menu card of Dishes")
-        display_veg_food()
+        print("Select the veg-dish from menu card.")
         user_veg = str(input("What's your order in veg-dishes ? \nEnter the dish name and no of dishes: "))
         size = int(input())
         hotel_customer.veg_food(user_veg, size, customer_name)
 
+    # non_veg order functioning
+    def _cocktails_order():
+        print("Select the cocktail from menu card.")
+        user_non_veg = str(input("What's your order in Cocktails ? \nEnter the cocktail name and no of cocktail: "))
+        size = int(input())
+        hotel_customer.non_veg_cocktail(user_non_veg, size, customer_name)
+
+    def non_veg_breakfast_order():
+        print("Select the non-veg-dish from menu card.")
+        user_non_veg = str(input("What's your order in non-veg breakfast ? \nEnter the dish name and no of dishes: "))
+        size = int(input())
+        hotel_customer.non_veg_breakfast(user_non_veg, size, customer_name)
+
+    def non_veg_dishes_order():
+        print("Select the non-veg-dish from menu card.")
+        user_non_veg = str(input("What's your order in non-veg dish ? \nEnter the dish name and no of dishes: "))
+        size = int(input())
+        hotel_customer.non_veg_food(user_non_veg, size, customer_name)
 
     main()
